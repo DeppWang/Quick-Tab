418dbd05b42bb30bc111a3e55ff3c02f73726ed1
DeppWang <deppwxq@gmail.com>
2020-09-07 22:52:14 +0800

    First add

---
+++ translatorBookmarklet.js
@@ -0,0 +1,11 @@
+window.readlangHostname = window.readlangHostname || "readlang.com", function () {
+   if (window.console = window.console || {}, window.console.log = window.console.log || 
+    function () { }, window.console.time = window.console.time || 
+    function () { }, window.console.timeEnd = window.console.timeEnd || 
+    function () { }, window.readlangHostname = window.readlangHostname || 
+    "readlang.com", /\.pdf$/.test(window.location.href)) 
+    return void alert("PDF files are not currently supported by Readlang."); 
+    if (window.READLANG = window.READLANG || {}, !window.READLANG.loadingBookmarklet) { 
+      window.READLANG.loadingBookmarklet = !0, window.READLANG.gitCommit = "421437613e19eda59cc01331f60e5173c4a19e5e"; 
+      var o = document.createElement("script"); o.type = "text/javascript", o.src = "//" + readlangHostname + "/src/embeddedWebReader.js?bust=" + window.READLANG.gitCommit, document.body.appendChild(o), o.onload = function () { } } }();
+//# sourceMappingURL=translatorBookmarklet.js.map
